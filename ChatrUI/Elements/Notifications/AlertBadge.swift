fb174df1f9e1b11417b3d1567b3e1ec4d543d1ba
Brandon Shaw <unobrandon@mac.com>
2022-01-25 22:54:35 -0500

    Update AlertBadge.swift

--- ChatrUI/Elements/Notifications/AlertBadge.swift
+++ ChatrUI/Elements/Notifications/AlertBadge.swift
@@ -7,18 +7,22 @@
 
 import SwiftUI
 
-struct AlertBadge: View {
-    @Binding var count: Int
+public struct AlertBadge: View {
+    var count: Binding<Int>
+    
+    public init(count: Binding<Int>) {
+        self.count = count
+    }
 
-    var body: some View {
+    public var body: some View {
         HStack {
-            Text(String(count))
+            Text("\(count.wrappedValue)")
                 .foregroundColor(.white)
                 .fontWeight(.medium)
                 .font(.footnote)
                 .padding(.horizontal, 5)
         }
-        .background(Capsule().frame(height: 22).frame(minWidth: 22).foregroundColor(Color("alertRed")).shadow(color: Color("alertRed").opacity(0.75), radius: 5, x: 0, y: 5))
-        .opacity(count > 0 ? 1 : 0)
+        .background(Capsule().frame(height: 22).frame(minWidth: 22).foregroundColor(Color("alertRed")).shadow(color: Color("alertRed").opacity(0.75), radius: 4, x: 0, y: 3))
+        .opacity(count.wrappedValue > 0 ? 1 : 0)
     }
 }
